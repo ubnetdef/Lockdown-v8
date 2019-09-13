834da3e48a95e279739b4abeb6ec43cb3a640530
l1ghtman2k <aibek.zhil@gmail.com>
2019-09-13 16:03:55 -0400

    15 teams deployment

--- inventory.py
+++ inventory.py
@@ -50,7 +50,7 @@ class Inventory(object):
         all_vars['pfsense_dns'] = all_vars['prefered_DNS']
         all_vars['Upstream_gateway_start_address'] = '192.168.253.1'
         all_vars['start_team'] = 1
-        all_vars['total_teams'] = 3
+        all_vars['total_teams'] = 15
         all_vars['afinity_enable'] = True
         all_vars['pfsense_template'] = 'Router-Internal-v0'
         all_vars['cloud_folder'] = '{}_Cloud'.format(all_vars['parent_folder'])
