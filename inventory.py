9af0b1204a3d567217b7c2784c4449c9e4b528eb
l1ghtman2k <aibek.zhil@gmail.com>
2019-10-15 02:13:48 -0400

    Lockdown v7 test release, get ready for "Try Deploy"s

--- inventory.py
+++ inventory.py
@@ -45,36 +45,41 @@ class Inventory(object):
         all_vars['datastore'] = 'THE-VAULT'
         all_vars['datacenter'] = 'UBNetDef'
         all_vars['cluster'] = 'MAIN'
-        all_vars['parent_folder'] = 'Internal-v0'
+        all_vars['parent_folder'] = 'Lockdown-v7'
         all_vars['WAN_start_address'] = '192.168.253.2'
         all_vars['pfsense_dns'] = all_vars['prefered_DNS']
         all_vars['Upstream_gateway_start_address'] = '192.168.253.1'
-        all_vars['start_team'] = 2
-        all_vars['total_teams'] = 1
+        all_vars['start_team'] = 1
+        all_vars['total_teams'] = 15
         all_vars['afinity_enable'] = True
-        all_vars['pfsense_template'] = 'Router-Internal-v0'
+        all_vars['pfsense_template'] = 'Router-v7'
         all_vars['cloud_folder'] = '{}_Cloud'.format(all_vars['parent_folder'])
-        all_vars['domain'] = 'star.wars'
-        all_vars['netbios'] = 'STARWARS'
+        all_vars['domain'] = 'hackercoin.lockdown'
+        all_vars['netbios'] = 'CRYPTO'
         all['vars'] = all_vars
         #####################################################################################################
 
         #####################################################################################################
         Active_Directory = ['10.X.1.60'.replace('X', str(i))for i in range(all_vars['start_team'], all_vars['total_teams'] + 1)]
+        Windows_clients_A = ['10.X.1.70'.replace('X', str(i))for i in range(all_vars['start_team'], all_vars['total_teams'] + 1)]
+        Windows_clients_B = ['10.X.1.50'.replace('X', str(i))for i in range(all_vars['start_team'], all_vars['total_teams'] + 1)]
         Linux_clients_A = ['10.X.1.10'.replace('X', str(i))for i in range(all_vars['start_team'], all_vars['total_teams'] + 1)]
         Linux_clients_B = ['10.X.1.20'.replace('X', str(i))for i in range(all_vars['start_team'], all_vars['total_teams'] + 1)]
         Linux_clients_C = ['10.X.1.30'.replace('X', str(i))for i in range(all_vars['start_team'], all_vars['total_teams'] + 1)]
-        Windows_clients_A = ['10.X.1.40'.replace('X', str(i))for i in range(all_vars['start_team'], all_vars['total_teams'] + 1)]
-        Windows_clients_B = ['10.X.1.50'.replace('X', str(i))for i in range(all_vars['start_team'], all_vars['total_teams'] + 1)]
+        Linux_clients_D = ['10.X.1.40'.replace('X', str(i))for i in range(all_vars['start_team'], all_vars['total_teams'] + 1)]
         WEB = ['10.X.2.2'.replace('X', str(i))for i in range(all_vars['start_team'], all_vars['total_teams'] + 1)]
         DB = ['10.X.2.3'.replace('X', str(i))for i in range(all_vars['start_team'], all_vars['total_teams'] + 1)]
         FTP = ['10.X.2.4'.replace('X', str(i))for i in range(all_vars['start_team'], all_vars['total_teams'] + 1)]
         GitLab = ['10.X.2.5'.replace('X', str(i))for i in range(all_vars['start_team'], all_vars['total_teams'] + 1)]
+        Security_Onion = ['10.X.1.250'.replace('X', str(i))for i in range(all_vars['start_team'], all_vars['total_teams'] + 1)]
+
+
         cloud = WEB + DB + FTP + GitLab
         for host_list in [
                 Active_Directory, Linux_clients_A, Linux_clients_B,
-                Linux_clients_C, Windows_clients_A, Windows_clients_B,
-                WEB, DB, FTP, GitLab
+                Linux_clients_C, Linux_clients_D,
+                Windows_clients_A, Windows_clients_B,
+                WEB, DB, FTP, GitLab, Security_Onion
         ]:
             for idx, host in enumerate(host_list):
                 team_number = idx + all_vars['start_team']
@@ -86,73 +91,82 @@ class Inventory(object):
                     hostvars[host]['domain_name'] = all_vars['domain']
                     hostvars[host]['netbios_name'] = all_vars['netbios']
                     hostvars[host]['ad_backuppass'] = 'Change.me!'
-                    hostvars[host]['template'] = "Windows-Server-AD-Internal-v0"
+                    hostvars[host]['template'] = "AD-Server-Windows2019-v7"
                     hostvars[host]['AD_Name'] = 'AD'
 
                 if host in Linux_clients_A:
-                    hostvars[host]['template'] = 'Desktop-XUbuntu-16.04-Internal-v0'
+                    hostvars[host]['template'] = 'Desktop-Elementary-5.0-Juno-v7'
                     hostvars[host]['AD_Name'] = 'Linux-A'
 
                 if host in Linux_clients_B:
-                    hostvars[host]['template'] = 'Desktop-CentOS-7-Internal-v0'
+                    hostvars[host]['template'] = 'Desktop-CentOS-7-v7'
                     hostvars[host]['AD_Name'] = 'Linux-B'
 
                 if host in Linux_clients_C:
-                    hostvars[host]['template'] = 'Desktop-LUbuntu-16.04-Internal-v0'
+                    hostvars[host]['template'] = 'Desktop-Mint-19.2-v7'
                     hostvars[host]['AD_Name'] = 'Linux-C'
 
+                if host in Linux_clients_D:
+                    hostvars[host]['template'] = 'Desktop-Fedora-30-v7'
+                    hostvars[host]['AD_Name'] = 'Linux-D'
+
+                if host in Security_Onion:
+                    hostvars[host]['template'] = 'Security-Onion-v7'
+                    hostvars[host]['AD_Name'] = 'Security-Onion'
+                    hostvars[host]['skip_deployment_check'] = 'True'
+
                 if host in Windows_clients_A:
-                    hostvars[host]['template'] = 'Desktop-Windows-10-Internal-v0'
+                    hostvars[host]['template'] = 'Desktop-Windows-10-v7'
                     hostvars[host]['AD_Name'] = 'Windows-A'
 
                 if host in Windows_clients_B:
-                    hostvars[host]['template'] = 'Desktop-Windows-7-Internal-v0'
+                    hostvars[host]['template'] = 'Desktop-Windows-7-v7'
                     hostvars[host]['AD_Name'] = 'Windows-B'
 
                 if host in FTP:
-                    hostvars[host]['template'] = 'Windows-Server-FTP-Internal-v0'
+                    hostvars[host]['template'] = 'Windows-Server-FTP-v7'
                     hostvars[host]['AD_Name'] = 'FTP'
 
                 if host in GitLab:
-                    hostvars[host]['template'] = 'Server-CentOS-7-GIT-Internal-v0'
+                    hostvars[host]['template'] = 'Git-Server-Centos-7-v7'
                     hostvars[host]['AD_Name'] = 'GitLab'
                     hostvars[host][
                         'gitlab_external_url'] = "http://gitlab.{}".format(all_vars['domain'])
 
                 if host in WEB:
-                    hostvars[host]['template'] = 'Server-Ubuntu-16.04-Internal-v0'
+                    hostvars[host]['template'] = 'Web-Server-CentOS-7-v7'
                     hostvars[host]['AD_Name'] = 'WEB'
-                    hostvars[host]['magento_host'] = "http://10.{}.2.2/".format(team_number)
-                    hostvars[host]['magento_db_host'] = "10.{}.2.3".format(team_number)
-                    hostvars[host]['apache_mods_enabled'] = ['rewrite.load']
+                    hostvars[host]['wiki'] = {
+                        'Name': 'CryptoWiki!',
+                        'user': 'admin',
+                        'pass': 'changeme',
+                        'dbuser': 'wiki',
+                        'dbpass': 'changeme',
+                        'dbname': 'mediawiki',
+                        'dbserver': DB[idx],
+                        'server':  "http://{}".format(host),
+                        'crypto_path': '../shared-files/Logos.png'
+                    }
 
                 if host in DB:
-                    hostvars[host]['template'] = 'Server-Ubuntu-16.04-Internal-v0'
+                    hostvars[host]['template'] = 'Web-Server-CentOS-7-v7'
                     hostvars[host]['AD_Name'] = 'DATABASE'
                     hostvars[host]['mysql_users'] = []
-                    hostvars[host]['mysql_users'].append({
-                        'name':
-                        'magento',
-                        'host':
-                        '%',
-                        'password':
-                        'changeme',
-                        'priv':
-                        '*.*:ALL,GRANT'
+                    hostvars[host]['mariadb_group_users'].append({
+                        'name': 'wiki',
+                        'host': '%',
+                        'password': 'changeme',
+                        'priv': '*.*:ALL,GRANT'
                     })
                     hostvars[host]['mysql_users'].append({
-                        'name':
-                        'admin',
-                        'host':
-                        '%',
-                        'password':
-                        'changeme',
-                        'priv':
-                        '*.*:ALL,GRANT'
+                        'name': 'admin',
+                        'host': '%',
+                        'password': 'changeme',
+                        'priv': '*.*:ALL,GRANT'
                     })
                     hostvars[host]['mysql_databases'] = []
-                    hostvars[host]['mysql_databases'].append({
-                        'name': 'magento',
+                    hostvars[host]['mariadb_host_databases'].append({
+                        'name': 'mediawiki',
                         'collation': 'utf8_general_ci',
                         'encoding': 'utf8',
                         'replicate': 0
@@ -161,8 +175,7 @@ class Inventory(object):
                 if host in Active_Directory + FTP:
                     hostvars[host]['ansible_user'] = 'Administrator'
                     hostvars[host]['ansible_password'] = 'Change.me!'
-                    hostvars[host]['customization']['password'] = hostvars[
-                        host]['ansible_password']
+                    hostvars[host]['customization']['password'] = hostvars[host]['ansible_password']
 
                 if host in Windows_clients_A + Windows_clients_B:
                     hostvars[host]['ansible_user'] = 'Admin'
@@ -171,11 +184,11 @@ class Inventory(object):
                 if host in WEB + DB:
                     hostvars[host]['ansible_python_interpreter'] = '/usr/bin/python3'
 
-                if host in Linux_clients_A + Linux_clients_B + Linux_clients_C + GitLab + WEB + DB:
+                if host in Linux_clients_A + Linux_clients_B + Linux_clients_C + Linux_clients_D + Security_Onion + GitLab + WEB + DB:
                     hostvars[host]['ansible_user'] = 'sysadmin'
                     hostvars[host]['ansible_password'] = 'changeme'
 
-                if host in Linux_clients_A + Linux_clients_B + Linux_clients_C + GitLab + WEB + DB:
+                if host in Linux_clients_A + Linux_clients_B + Linux_clients_C + Linux_clients_D + Security_Onion + GitLab + WEB + DB:
                     hostvars[host]['ansible_become_pass'] = hostvars[host]['ansible_password']
                     hostvars[host]['OS'] = 'Linux'
 
@@ -206,19 +219,24 @@ class Inventory(object):
                     hostvars[host]['folder'] = 'Lockdown/{}/Team{:02d}'.format(
                         all_vars['parent_folder'], team_number)
 
-                network['name'] = "team{}-net".format(team_number)
-                network['ip'] = host
-                network['netmask'] = '255.255.255.0'
-                network['gateway'] = "10.{}.{}.1".format(
-                    team_number,
-                    host.split('.')[2])
-                network['start_connected'] = True
-                hostvars[host]['networks'] = [network]
+
+                def networking (ip):
+                    network['name'] = "team{}-net".format(team_number)
+                    network['ip'] = ip
+                    network['netmask'] = '255.255.255.0'
+                    network['gateway'] = "10.{}.{}.1".format(team_number, host.split('.')[2])
+                    network['start_connected'] = True
+                    return network
+
+
+                hostvars[host]['networks'] = [networking(host)]
                 hostvars[host]['team_number'] = team_number
-                hostvars[host]['team_number'] = team_number
-                # hostvars[host]['customization']['existing_vm'] = True
-                hostvars[host]['networks'] = [network]
                 hostvars[host]['customization']['hostname'] = hostvars[host]['AD_Name']
+
+                if host in Security_Onion:
+                    hostvars[host]['networks'] = []
+                    for net_instance in ['13.33.33.37', '13.33.33.38', '13.33.33.39']:
+                        hostvars[host]['networks'].append(networking(net_instance))
 
         AD = {}
         AD['hosts'] = Active_Directory
@@ -228,6 +246,10 @@ class Inventory(object):
         Linux_B['hosts'] = Linux_clients_B
         Linux_C = {}
         Linux_C['hosts'] = Linux_clients_C
+        Linux_D = {}
+        Linux_D['hosts'] = Linux_clients_D
+        Sec_Onion = {}
+        Sec_Onion['hosts'] = Security_Onion
         Windows_A = {}
         Windows_A['hosts'] = Windows_clients_A
         Windows_B = {}
@@ -247,6 +269,8 @@ class Inventory(object):
         inventory['Linux-A'] = Linux_A
         inventory['Linux-B'] = Linux_B
         inventory['Linux-C'] = Linux_C
+        inventory['Linux-D'] = Linux_D
+        inventory['Security-Onion'] = Sec_Onion
         inventory['Windows-A'] = Windows_A
         inventory['Windows-B'] = Windows_B
         inventory['WEB'] = WEB_Servers
