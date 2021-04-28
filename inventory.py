20e1ccf663e435262e0e2f2da853e89bece50822
L1ghtman2k <35672535+L1ghtman2k@users.noreply.github.com>
2021-04-28 07:25:12 -0400

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -49,8 +49,8 @@ class Inventory(object):
         all_vars['WAN_start_address'] = '192.168.253.2'
         all_vars['pfsense_dns'] = all_vars['prefered_DNS']
         all_vars['Upstream_gateway_start_address'] = '192.168.253.1'
-        all_vars['start_team'] = 1
-        all_vars['total_teams'] = 13
+        all_vars['start_team'] = 14
+        all_vars['total_teams'] = 2
         all_vars['Lockdown_user_role'] = 'Lockdown blue team'
         all_vars['afinity_enable'] = True
         all_vars['pfsense_template'] = 'Router-v8'
@@ -267,11 +267,11 @@ class Inventory(object):
         inventory['all'] = all
 
         #inventory['Active_Directory'] = Active_Directory_dict
-        #inventory['Nurse_Station_A'] = Nurse_Station_A_dict
-        #inventory['Nurse_Station_B'] = Nurse_Station_B_dict
-        #inventory['Doctor_Station'] = Doctor_Station_dict
-        #inventory['Secretary_Station'] = Secretary_Station_dict
-        #inventory['IT_Station'] = IT_Station_dict
+        inventory['Nurse_Station_A'] = Nurse_Station_A_dict
+        inventory['Nurse_Station_B'] = Nurse_Station_B_dict
+        inventory['Doctor_Station'] = Doctor_Station_dict
+        inventory['Secretary_Station'] = Secretary_Station_dict
+        inventory['IT_Station'] = IT_Station_dict
         #inventory['Ubuntu'] = Ubuntu_dict
         inventory['FTP'] = FTP_dict
         #inventory['WEB'] = WEB_dict
