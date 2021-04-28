17ec8b1da4fb9c4de848da2d59707c1136d47f82
L1ghtman2k <35672535+L1ghtman2k@users.noreply.github.com>
2021-04-28 01:54:00 -0400

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -267,11 +267,11 @@ class Inventory(object):
         inventory['all'] = all
 
         #inventory['Active_Directory'] = Active_Directory_dict
-        inventory['Nurse_Station_A'] = Nurse_Station_A_dict
-        inventory['Nurse_Station_B'] = Nurse_Station_B_dict
-        inventory['Doctor_Station'] = Doctor_Station_dict
-        inventory['Secretary_Station'] = Secretary_Station_dict
-        inventory['IT_Station'] = IT_Station_dict
+        #inventory['Nurse_Station_A'] = Nurse_Station_A_dict
+        #inventory['Nurse_Station_B'] = Nurse_Station_B_dict
+        #inventory['Doctor_Station'] = Doctor_Station_dict
+        #inventory['Secretary_Station'] = Secretary_Station_dict
+        #inventory['IT_Station'] = IT_Station_dict
         #inventory['Ubuntu'] = Ubuntu_dict
         inventory['FTP'] = FTP_dict
         #inventory['WEB'] = WEB_dict
