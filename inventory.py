1bd984e43d718654075ff69e6816442f7e225c13
Vasu-Baldwa <35380065+Vasu-Baldwa@users.noreply.github.com>
2022-04-26 00:50:19 -0400

    Please Windows

--- inventory.py
+++ inventory.py
@@ -276,16 +276,16 @@ class Inventory(object):
 
         inventory['all'] = all
 
-        #inventory['Active_Directory'] = Active_Directory_dict
-        #inventory['Windows1'] = Windows1_dict
-        #inventory['Windows2'] = Windows2_dict
+        inventory['Active_Directory'] = Active_Directory_dict
+        inventory['Windows1'] = Windows1_dict
+        inventory['Windows2'] = Windows2_dict
         #inventory['AliExpressWindows'] = AliExpressWindows_dict
         #inventory['Ubuntu'] = Ubuntu_dict
         #inventory['Ubuntu2'] = Ubuntu2_dict
         #inventory['CentOs'] = CentOS_dict
 
-        #inventory['FTP'] = FTP_dict
-        inventory['WEB'] = WEB_dict
+        inventory['FTP'] = FTP_dict
+        #inventory['WEB'] = WEB_dict
         #inventory['DB'] = DB_dict
         #inventory['WebAPI'] = API_dict
 
