0b3bd36176394d0bba02e07fbf0afe077e92555b
Vasu-Baldwa <35380065+Vasu-Baldwa@users.noreply.github.com>
2022-04-25 22:52:33 -0400

    Just database. Please

--- inventory.py
+++ inventory.py
@@ -279,15 +279,15 @@ class Inventory(object):
         #inventory['Active_Directory'] = Active_Directory_dict
         #inventory['Windows1'] = Windows1_dict
         #inventory['Windows2'] = Windows2_dict
-        inventory['AliExpressWindows'] = AliExpressWindows_dict
-        inventory['Ubuntu'] = Ubuntu_dict
-        inventory['Ubuntu2'] = Ubuntu2_dict
+        #inventory['AliExpressWindows'] = AliExpressWindows_dict
+        #inventory['Ubuntu'] = Ubuntu_dict
+        #inventory['Ubuntu2'] = Ubuntu2_dict
         #inventory['CentOs'] = CentOS_dict
 
         #inventory['FTP'] = FTP_dict
-        inventory['WEB'] = WEB_dict
+        #inventory['WEB'] = WEB_dict
         inventory['DB'] = DB_dict
-        inventory['WebAPI'] = API_dict
+        #inventory['WebAPI'] = API_dict
 
         #################################################################################################
 
