8d484d2fe4b002e2e3defa49b56737140f193601
Vasu-Baldwa <35380065+Vasu-Baldwa@users.noreply.github.com>
2022-12-03 11:35:09 -0500

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -49,9 +49,9 @@ class Inventory(object):
         all_vars['WAN_start_address'] = '192.168.253.2'
         all_vars['pfsense_dns'] = all_vars['prefered_DNS']
         all_vars['Upstream_gateway_start_address'] = '192.168.253.1'
-        all_vars['start_team'] = 14
+        all_vars['start_team'] = 1
         #todo Change number of teams
-        all_vars['total_teams'] = 1
+        all_vars['total_teams'] = 15
         all_vars['Lockdown_user_role'] = 'Lockdown blue team'
         all_vars['afinity_enable'] = True
         all_vars['pfsense_template'] = 'Router-v8'
@@ -288,13 +288,13 @@ class Inventory(object):
         #inventory['Windows1'] = Windows1_dict
         #inventory['Windows2'] = Windows2_dict
         inventory['Ubuntu1'] = Ubuntu1_dict
-        inventory['Ubuntu2'] = Ubuntu2_dict
+        #inventory['Ubuntu2'] = Ubuntu2_dict
         #inventory['Ubuntu3'] = Ubuntu3_dict
         #inventory['CentOs'] = CentOS_dict
 
-        inventory['FTP'] = FTP_dict
+        #inventory['FTP'] = FTP_dict
         #inventory['WEB'] = WEB_dict
-        inventory['DB'] = DB_dict
+        #inventory['DB'] = DB_dict
         #inventory['DevServer'] = DevServer_dict
 
         #################################################################################################
