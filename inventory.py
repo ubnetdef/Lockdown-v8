0337b4bbc255b9da451d453d8d7053a5e5e12828
3th4nn <104602718+3th4nn@users.noreply.github.com>
2023-11-26 14:41:15 -0500

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -284,18 +284,18 @@ class Inventory(object):
 
         inventory['all'] = all
 
-        #inventory['Active_Directory'] = Active_Directory_dict
-        #inventory['Windows1'] = Windows1_dict
-        #inventory['Windows2'] = Windows2_dict
-        #inventory['Ubuntu1'] = Ubuntu1_dict
-        #inventory['Ubuntu2'] = Ubuntu2_dict
+        inventory['Active_Directory'] = Active_Directory_dict
+        inventory['Windows1'] = Windows1_dict
+        inventory['Windows2'] = Windows2_dict
+        inventory['Ubuntu1'] = Ubuntu1_dict
+        inventory['Ubuntu2'] = Ubuntu2_dict
         #inventory['Ubuntu3'] = Ubuntu3_dict
         #inventory['CentOs'] = CentOS_dict
 
-        #inventory['FTP'] = FTP_dict
+        inventory['FTP'] = FTP_dict
         inventory['WEB'] = WEB_dict
-        #inventory['DB'] = DB_dict
-        #inventory['DevServer'] = DevServer_dict
+        inventory['DB'] = DB_dict
+        inventory['DevServer'] = DevServer_dict
 
         #################################################################################################
 
