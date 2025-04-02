d96f8480c58b4e506aad35e074cee68c46745f6c
JayModded <78915475+JayModded0@users.noreply.github.com>
2025-04-02 13:52:10 -0400

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -49,7 +49,7 @@ class Inventory(object):
         all_vars['WAN_start_address'] = '192.168.253.2'
         all_vars['pfsense_dns'] = all_vars['prefered_DNS']
         all_vars['Upstream_gateway_start_address'] = '192.168.253.1'
-        all_vars['start_team'] = 15
+        all_vars['start_team'] = 14
         #todo Change number of teams
         all_vars['total_teams'] = 1
         all_vars['Lockdown_user_role'] = 'Lockdown blue team'
