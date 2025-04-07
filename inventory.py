50a7cd5be2a44bd2d444eebcf2bd07c4ff68eb62
JayModded <78915475+JayModded0@users.noreply.github.com>
2025-04-07 13:25:10 -0400

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -49,9 +49,9 @@ class Inventory(object):
         all_vars['WAN_start_address'] = '192.168.253.2'
         all_vars['pfsense_dns'] = all_vars['prefered_DNS']
         all_vars['Upstream_gateway_start_address'] = '192.168.253.1'
-        all_vars['start_team'] = 13
+        all_vars['start_team'] = 1
         #todo Change number of teams
-        all_vars['total_teams'] = 1
+        all_vars['total_teams'] = 15
         all_vars['Lockdown_user_role'] = 'Lockdown blue team'
         all_vars['afinity_enable'] = True
         all_vars['pfsense_template'] = 'Router-v18'
@@ -292,12 +292,12 @@ class Inventory(object):
         inventory['Ubuntu1'] = Ubuntu1_dict
         inventory['Ubuntu2'] = Ubuntu2_dict
         #inventory['Ubuntu3'] = Ubuntu3_dict
-        #inventory['WebApp'] = CentOS_dict
+        inventory['WebApp'] = CentOS_dict
 
-        #inventory['UbuntuFTP'] = FTP_dict
-        #inventory['BackupServer'] = WEB_dict
+        inventory['UbuntuFTP'] = FTP_dict
+        inventory['BackupServer'] = WEB_dict
         #inventory['DB'] = DB_dict
-        #inventory['DevServer'] = DevServer_dict
+        inventory['DevServer'] = DevServer_dict
 
         #################################################################################################
 
